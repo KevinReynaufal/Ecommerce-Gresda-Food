ec37f61850e69a8b46e5a75cd312af861599abef
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2024-01-25 05:06:25 +0700

    Change db name

--- Assets/Config/config.php
+++ Assets/Config/config.php
@@ -8,7 +8,7 @@ define('SITEURL', 'http://localhost/Ecommerce-Gresda-Food/');
 define('LOCALHOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'gresda-food');
+define('DB_NAME', 'gresda_food');
 
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
 $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
